a42d5fb61124ad38141bd43d55939b302b42cc2f
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-06-26 12:28:59 +0200

    Add task

---
+++ src/lib/constants.js
@@ -0,0 +1,22 @@
+export const colors = [
+	{ hexa: "#E2A5FF", id: "1" },
+	{ hexa: "#B1FFA5", id: "2" },
+	{ hexa: "#FFBCA7", id: "3" },
+	{ hexa: "#A7F4FF", id: "4" },
+	{ hexa: "#A7BAFF", id: "5" },
+	{ hexa: "#FFA7EC", id: "6" },
+	{ hexa: "#76A675", id: "7" },
+	{ hexa: "#4AD4CC", id: "8" },
+	{ hexa: "#D83684", id: "9" },
+	{ hexa: "#F671F9", id: "10" },
+	{ hexa: "#FF7C7C", id: "11" },
+	{ hexa: "#2C93CD", id: "12" },
+	];
+	
+	export const text = [
+	{ text: "Jeux"},
+	{ text: "Design"},
+	{ text: "Rendez-vous"},
+	{ text: "Travail"},
+	{ text: "Tache"},
+	];
